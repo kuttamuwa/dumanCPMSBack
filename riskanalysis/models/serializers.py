13d6fa1be357f994ccbd8215b9a0a0ca8a987a4d
kuttamuwa <ucok.umut@gmail.com>
2021-01-16 13:55:26 +0300

    risk analiz serialize api

--- riskanalysis/models/serializers.py
+++ riskanalysis/models/serializers.py
@@ -6,9 +6,10 @@ from riskanalysis.models.models import DataSetModel, RiskDataSetPoints
 class DatasetSerializer(serializers.ModelSerializer):
     class Meta:
         model = DataSetModel
+        fields = '__all__'
 
 
 class RiskPointsSerializer(serializers.ModelSerializer):
     class Meta:
         model = RiskDataSetPoints
-
+        fields = '__all__'
--- riskanalysis/views/routers.py
+++ riskanalysis/views/routers.py
@@ -4,5 +4,5 @@ from riskanalysis.views.api import DatasetAPI, RiskPointsAPI
 
 router = routers.DefaultRouter()
 
-router.register(r'dataset/', DatasetAPI)
-router.register(r'points/', RiskPointsAPI)
+router.register(r'dataset', DatasetAPI)
+router.register(r'points', RiskPointsAPI)
