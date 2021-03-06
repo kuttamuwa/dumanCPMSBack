570216f2334c512916b5cf2aa23cc7eef70cb96f
kuttamuwa <ucok.umut@gmail.com>
2021-03-06 14:12:26 +0300

    dashboard api okay

--- checkaccount/controllers/admin.py
+++ checkaccount/controllers/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
-from checkaccount.models.models import CheckAccount, SysPersonnel, SysDepartments, PartnershipDocuments, Sectors
+from checkaccount.models.models import CheckAccount, SysPersonnel, SysDepartments, Sectors
 
 
 admin.site.register(CheckAccount)
 admin.site.register(SysPersonnel)
 admin.site.register(SysDepartments)
-admin.site.register(PartnershipDocuments)
+# admin.site.register(PartnershipDocuments)
 admin.site.register(Sectors)
